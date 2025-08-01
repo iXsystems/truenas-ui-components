9224d971e5b44290291906464a317a75d53ef25a
Damian Szidiropulosz <damianos@knoxbug.org>
2025-08-01 14:47:56 -0400

    Renamed examples directory to patterns

---
+++ projects/truenas-ui/src/stories/patterns/login/login.stories.ts
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { IxInputComponent } from '../../../lib/ix-input/ix-input.component';
+import { IxButtonComponent } from '../../../lib/ix-button/ix-button.component';
+import { InputType } from '../../../lib/enums/input-type.enum';
+import { within, userEvent } from '@storybook/testing-library';
+
+@Component({
+  selector: 'app-login-form-doc',
+  templateUrl: './login.example.html',
+  styleUrl: './login.example.scss',
+  standalone: true,
+  imports: [IxInputComponent, IxButtonComponent],
+})
+class LoginFormDocComponent {
+ InputType = InputType;
+}
+
+const meta: Meta<LoginFormDocComponent> = {
+  title: 'Patterns/Login Form',
+  component: LoginFormDocComponent,
+  tags: [],
+};
+
+export default meta;
+
+type Story = StoryObj<LoginFormDocComponent>;
+
+export const BasicFlow: Story = {
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    const usernameInput = await canvas.findByTestId('username');
+    const passwordInput = await canvas.findByTestId('password');
+    const submitButton = await canvas.getByRole('button');
+
+    await step('Enter email and password', async () => {
+      await userEvent.type(usernameInput, 'johndoe');
+      await new Promise((r) => setTimeout(r, 1500));
+      await userEvent.type(passwordInput, 'abcd1234');
+    });
+
+    await new Promise((r) => setTimeout(r, 1500));
+
+    await step('Submit form', async () => {
+      await userEvent.click(submitButton);
+    });
+
+  },
+};
+
